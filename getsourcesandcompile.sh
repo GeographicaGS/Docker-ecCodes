b97bfeec8156993c8a91927c9c589c8761caaaf6
cayetanobv <cayetano.benavent@geographica.gs>
2015-06-10 10:26:09 +0200

    remove sources and other files from image

--- getsourcesandcompile.sh
+++ getsourcesandcompile.sh
@@ -9,7 +9,8 @@ mkdir build
 cd build 
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local /tmp/eccodes/eccodes-0.11.0-Source
 make
-# comment make check if you want test library with data (test require download all test data and it takes a long time...) 
+# uncomment "make check" if you want test library with data (test require download all test data and it takes a long time...) 
 #make check
 make install
+cd /tmp && rm -R eccodes && rm getsourcesandcompile.sh
 exit
